28c78effa96db8f49fceb7be8c358dc751f25782
Abdul-Logiclion <abduljabarsc01@gmail.com>
2025-08-23 01:02:46 +0500

    done

--- app/components/Portfolio.tsx
+++ app/components/Portfolio.tsx
@@ -106,7 +106,7 @@ export default function Portfolio() {
 
   return (
     <div className="min-h-screen w-full flex justify-center items-center bg-white font-sans">
-      <main className="mt-9 md:h-[80vh] md:w-[70vw] overflow-auto border border-gray-200 shadow-lg bg-white rounded-lg">
+      <main className="mt-9 md:h-[80vh] md:w-[68vw] overflow-auto border border-gray-200 shadow-lg bg-white rounded-lg">
         <HeroSection />
         <AboutSection />
         <SkillsSection skills={skills} />
